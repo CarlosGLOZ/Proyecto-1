da49f4e12a89e6247e1ee4a7f01f3c3baa0f4f52
Sergi <setapeses@gmail.com>
2022-11-07 19:14:51 +0100

    config sql names

--- config/config.php
+++ config/config.php
@@ -12,29 +12,58 @@ const BD =
         'TABLA' => 'tbl_camareros',
         'ID' => 'id_camarero',
         'NOMBRE' => 'nom_camarero',
-        'PASSWORD' => 'password_camarero'
+        'PASSWORD' => 'password_camarero',
+        'DNI' => 'dni_empleado',
+        'CARGO' => 'fk_cargo_empleado',
+        'PASSWORD' => 'password_empleado'
     ],
 
     'SALA' =>
     [
         'TABLA' => 'tbl_sala',
-        'ID' => 'id_sala'
+        'ID' => 'id_sala',
+        'NOMBRE' => 'nom_sala'
     ],
     
     'MESA' =>
     [
         'TABLA' => 'tbl_mesa',
         'ID' => 'id_mesa',
-        'CAPACIDAD' => 'capacidad_mesa',
         'ESTADO' => 'estado_mesa',
-        'SALA' => 'fk_num_sala'
+        'SALA' => 'fk_num_sala',
+        'CAPACIDAD' => 'capacidad_mesa'
     ],
 
     'REGISTRO' =>
     [
         'TABLA' => 'tbl_registros',
-        'ID' => 'id_registro'
+        'ID' => 'id_registro',
+        'FECHAENTRADA' => 'fecha_entrada',
+        'FECHASALIDA' => 'fecha_salida',
+        'MESA' => 'id_mesa',
+        'CAMARERO' => 'id_camarero',
+        'COMENSALES' => 'num_comensales'
     ],
+
+
+    'CARGO' =>
+    [
+        'TABLA' => 'tbl_cargo',
+        'ID' => 'id_cargo',
+        'NOMBRE' => 'nom_cargo'
+    ],
+
+
+    'INCIDENCIA' =>
+    [
+        'TABLA' => 'tbl_incidencia',
+        'ID' => 'id_incidencia',
+        'NOMBRE' => 'nom_inc',
+        'ESTADO' => 'estado_inc',
+        'MESA' => 'fk_mesa_inc'
+    ],
+
+
 ];
 
 const LOGIN_FORM = 
