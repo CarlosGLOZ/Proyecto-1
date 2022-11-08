ff5370a68eb5faf1bf4dd7a14cd03836cc70e51e
Sergi <setapeses@gmail.com>
2022-11-08 19:39:39 +0100

    exportv2

--- proc/export_register.php
+++ proc/export_register.php
@@ -2,11 +2,10 @@
 
 require_once '../config/conexion.php';
 
-
 mysqli_select_db($conexion, 'crud');  
-$sql = "SELECT * FROM `tbl_registro`;";  
+$sql = "SELECT `id_registro`, `fecha_entrada`, `fecha_salida`, `id_mesa`, `nom_empleado`, `num_comensales` FROM `tbl_registro` INNER JOIN tbl_empleado on tbl_registro.id_camarero=tbl_empleado.id_empleado";  
 $setRec = mysqli_query($conexion, $sql);  
-$columnHeader = '';  
+$columnHeader = "";  
 $columnHeader = "ID Registro" . "\t" . "Fecha Entrada" . "\t" . "Fecha Salida" . "\t" . "Mesa" . "\t". "Camarero" . "\t". "Comensales" . "\t";  
 $setData = '';  
   while ($rec = mysqli_fetch_row($setRec)) {  
@@ -24,4 +23,7 @@ header("Pragma: no-cache");
 header("Expires: 0");  
 
   echo ucwords($columnHeader) . "\n" . $setData . "\n";  
+
+  redirect('../view/registros.php');
+
  ?> 
