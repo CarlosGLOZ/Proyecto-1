ea23352e7293105e5dc3066722e51609333de4a7
CarlosGLOZ <100007082.joan23@fje.edu>
2022-11-11 16:57:12 +0100

    filtros retocados

--- view/index.php
+++ view/index.php
@@ -107,8 +107,8 @@
         <input type="hidden" id="<?php echo FILTROS['SALA'];?>" value="<?php echo $filtros[FILTROS['SALA']]?>">
         
         <div class="nav-item">
-            <button class="btn btn-success" onclick="enviarFiltros('<?php echo $url_base;?>', [<?php echo FILTROS['SALA'].', '.FILTROS['CAPACIDAD'].', '.FILTROS['DISPONIBILIDAD'];?>]);">Filtrar</button>
-            <button class="btn btn-danger" onclick="limpiarFiltros('<?php echo $url_base.'?'.FILTROS['SALA'].'='.$filtros[FILTROS['SALA']];?>');">Limpiar Filtros</button>
+            <button class="btn btn-outline-success" onclick="enviarFiltros('<?php echo $url_base;?>', [<?php echo FILTROS['SALA'].', '.FILTROS['CAPACIDAD'].', '.FILTROS['DISPONIBILIDAD'];?>]);">Filtrar</button>
+            <button class="btn btn-outline-danger" onclick="limpiarFiltros('<?php echo $url_base.'?'.FILTROS['SALA'].'='.$filtros[FILTROS['SALA']];?>');">Limpiar Filtros</button>
         </div>
     </div>
 </div>
@@ -183,14 +183,14 @@
       // Mostrar botones según estado de la mesa
       echo "<div class='text-center github-link'>";
       if ($mesa[BD['MESA']['ESTADO']] == 0) {
-        echo "<button class='btn btn-success' onclick='abrirModalOcupado(".$mesa[BD['MESA']['ID']].")'>Ocupar</button> ";
-        echo "<button class='btn btn-secondary' onclick='abrirModalMantenimiento(".$mesa[BD['MESA']['ID']].")'>Mantenimiento</button>";
+        echo "<button class='btn btn-outline-success' onclick='abrirModalOcupado(".$mesa[BD['MESA']['ID']].")'>Ocupar</button> ";
+        echo "<button class='btn btn-outline-secondary' onclick='abrirModalMantenimiento(".$mesa[BD['MESA']['ID']].")'>Mantenimiento</button>";
       } elseif ($mesa[BD['MESA']['ESTADO']] == 1) {
-        echo "<button class='btn btn-danger' onclick='abrirModalLiberar(".$mesa[BD['MESA']['ID']].")'>Liberar</button> ";
-        echo "<button class='btn btn-secondary' onclick='abrirModalMantenimiento(".$mesa[BD['MESA']['ID']].")'>Mantenimiento</button>";
+        echo "<button class='btn btn-outline-danger' onclick='abrirModalLiberar(".$mesa[BD['MESA']['ID']].")'>Liberar</button> ";
+        echo "<button class='btn btn-outline-secondary' onclick='abrirModalMantenimiento(".$mesa[BD['MESA']['ID']].")'>Mantenimiento</button>";
       } elseif ($mesa[BD['MESA']['ESTADO']] == 2) {
-        echo "<button class='btn btn-success' onclick='abrirModalOcupado(".$mesa[BD['MESA']['ID']].")'>Ocupar</button> ";
-        echo "<button class='btn btn-danger' onclick='abrirModalLiberar(".$mesa[BD['MESA']['ID']].")'>Liberar</button>";
+        echo "<button class='btn btn-outline-success' onclick='abrirModalOcupado(".$mesa[BD['MESA']['ID']].")'>Ocupar</button> ";
+        echo "<button class='btn btn-outline-danger' onclick='abrirModalLiberar(".$mesa[BD['MESA']['ID']].")'>Liberar</button>";
       }
       
       echo "</div></div>
